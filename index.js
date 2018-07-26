9690e367a528d13311c361aed98307c2422cb932
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-07-26 22:19:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution in this file.
 
 function lowerCaseDrivers(object){
-  return object.map(function(name){}
-    )
+  return object.map(function(name){
+    return name.toLowerCase();
+  })
+    
 }
