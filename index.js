219a48ba71ca5a6ccdd04a9ecf9f920d6e9d9983
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-07-26 22:23:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ function nameToAttributes (object){
   return object.map(function(driver){
     let firstName=driver.slice(' ')[0]
     let lastName=driver.slice(' ')[1]
+    return 'firstName'
   })
 }
